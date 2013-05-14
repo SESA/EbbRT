863b550332f3f84835a98b3554accba26a796aa3
James Cadden <jmcadden@bu.edu>
2013-05-14 18:04:17 -0400

    Comments added throughout the initial boot functions (doxygen formatting)

--- src/lrt/boot.cpp
+++ src/lrt/boot.cpp
@@ -8,6 +8,9 @@
 #include "lrt/mem.hpp"
 #include "lrt/trans.hpp"
 
+/** @brief Initialization of core lrt functionality.  Only the first core will
+ * execute
+ */
 void
 ebbrt::lrt::boot::init()
 {
@@ -16,16 +19,23 @@ ebbrt::lrt::boot::init()
 
   auto num_cores = event::get_num_cores();
 
+  /* Set up initial system state */
+  /* We spin indefinably if any lrt function fails to initialize. */
   if (mem::init(num_cores) &&
       event::init(num_cores) &&
       trans::init(num_cores)) {
-    init_smp(num_cores);
+    /* spin up remaining cores */
+    init_smp(num_cores); 
+   /* we should not return from init_smp() */
   }
 
   while (1)
     ;
 }
 
+/** @brief Finish cores initialization and enter application. Parallel
+ * execution!
+ */
 void
 ebbrt::lrt::boot::init_cpu()
 {
--- src/lrt/event.cpp
+++ src/lrt/event.cpp
@@ -5,14 +5,24 @@
 
 uintptr_t** ebbrt::lrt::event::altstack;
 
+/**
+ * @brief Initialize data structures for event handling 
+ *
+ * @param num_cores amount of cores spun-up on boot
+ *
+ * @return success/fail
+ */
 bool
 ebbrt::lrt::event::init(unsigned num_cores)
 {
   altstack = new (mem::malloc(sizeof(uintptr_t*) * num_cores, 0))
     uintptr_t*[num_cores];
-  return init_arch(num_cores);
+  return init_arch();
 }
 
+/**
+ * @brief Each core configured in preparation to receive events
+ */
 void
 ebbrt::lrt::event::init_cpu()
 {
