711b29d9f5d64283ecdd5067098343e841b9c3c5
Dan Schatzberg <schatzberg.dan@gmail.com>
2013-05-29 15:43:46 -0400

    Removed stray using directive

--- src/ebb/ebb.hpp
+++ src/ebb/ebb.hpp
@@ -8,7 +8,6 @@
 namespace ebbrt {
   using lrt::trans::EbbRef;
   using lrt::trans::EbbId;
-  using lrt::trans::NodeId;
   using lrt::trans::EbbRep;
   using lrt::trans::EbbRoot;
   using lrt::event::Location;
