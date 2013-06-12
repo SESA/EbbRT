2b4b5aab4f7a620e29106280f33fff67033f9b27
Orran Krieger <okrieg@gmail.com>
2013-06-12 09:57:59 -0400

    comment change to test modified build process

--- test/hello-bare.py
+++ test/hello-bare.py
@@ -8,7 +8,7 @@ for z in range(1, cores+1):
     sys.stdout.flush()
     child = pexpect.spawn ('qemu-system-x86_64 -nographic -smp ' + str(z) + ' src/app/HelloWorld/HelloWorld.iso')
     # uncomment following line to see results of experiment
-    #  child.logfile = sys.stdout
+    #   child.logfile = sys.stdout
     for j in range(z):
         child.expect('Hello World', timeout=100)
     # child.kill(9)
