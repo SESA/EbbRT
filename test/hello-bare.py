ff1eb551713a0e757df085c43295625f76f799da
Orran Krieger <okrieg@gmail.com>
2013-06-14 09:54:30 -0400

    added dependancies for scripts on test programs

--- test/hello-bare.py
+++ test/hello-bare.py
@@ -4,7 +4,7 @@ import sys
 
 cores = multiprocessing.cpu_count()
 for z in range(1, cores+1):
-    sys.stdout.write( "bindtst on " + str(z) + " cores [")
+    sys.stdout.write( "HelloWorld bare metal on " + str(z) + " cores [")
     sys.stdout.flush()
     child = pexpect.spawn ('qemu-system-x86_64 -nographic -smp ' + str(z) + ' src/app/HelloWorld/HelloWorld.iso')
     # uncomment following line to see results of experiment
