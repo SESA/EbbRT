4cfabac00f463479dd54a303cd1f5cfa6107eb8b
Orran Krieger <okrieg@gmail.com>
2013-07-20 16:23:16 -0400

    header defines macroes for early symbols

---
+++ src/lrt/bare/config.hpp
@@ -0,0 +1,64 @@
+/*
+  EbbRT: Distributed, Elastic, Runtime
+  Copyright (C) 2013 SESA Group, Boston University
+
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU Affero General Public License as
+  published by the Free Software Foundation, either version 3 of the
+  License, or (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU Affero General Public License for more details.
+
+  You should have received a copy of the GNU Affero General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+#ifndef LRT_BARE_CONFIG_HPP
+#define LRT_BARE_CONFIG_HPP
+
+
+namespace ebbrt {
+  namespace lrt {
+    
+    struct SymTabEntry {
+      /* 
+	 FIXME:function here should take an argument to section of
+	 flattened device tree, and should return void
+      */
+      ebbrt::EbbRoot* (*config_func)(); 
+      /* offset into the string table for each string*/
+      ptrdiff_t str_offset;
+    };
+
+    void dump_config_info();
+  }
+}
+
+
+/* 
+ * Table of strings for symbols needed by configuration
+ * at very early boot time
+ */
+extern char ebbstrtab_start[];
+extern char ebbstrtab_end[];
+extern struct ebbrt::lrt::SymTabEntry ebbsymtab_start[];
+/* 
+ * Symbol table for functions needed early in boot.
+ */
+extern struct ebbrt::lrt::SymTabEntry ebbsymtab_end[];
+
+#define ADD_EARLY_CONFIG_SYMBOL(name,symbol,func)	\
+char name[] __attribute__((section("ebbstrtab")))	\
+    = symbol;						\
+							\
+ebbrt::lrt::SymTabEntry					\
+name ## _entry __attribute__((section("ebbsymtab")))	\
+= {							\
+  .config_func = func,					\
+  .str_offset = name - ebbstrtab_start			\
+};
+
+#endif /*LRT_BARE_CONFIG_HPP*/
+
