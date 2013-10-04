2265de33ba5524d336812b61cda2415ea07683f2
Jim Cadden <jmcadden@bu.edu>
2013-10-04 12:49:57 -0400

    Misses & ebb calls working on both ulnx/bare w/ configuration
    Linux app requires the path to fdt BINARY passed to EbbRT instance on
    construction

--- src/app/HelloEbb/HelloEbb.cpp
+++ src/app/HelloEbb/HelloEbb.cpp
@@ -38,8 +38,6 @@
 
 
 #ifdef __ebbrt__
-bool ebbrt::app::multi = true;
-
 void
 ebbrt::app::start()
 {
@@ -53,26 +51,75 @@ ebbrt::app::start()
 }
 #endif
 
-#ifdef __linux__
-int main()
-{
-  ebbrt::EbbRT instance;
 
-  std::vector<std::thread> threads(std::thread::hardware_concurrency());
-  for (auto& thread : threads) {
-    thread = std::thread([&]{
-      ebbrt::Context context{instance};
-      context.Activate();
-      char str[] = "Hello Ebb\n";
-      auto buf = ebbrt::console->Alloc(sizeof(str));
-      std::memcpy(buf.data(), str, sizeof(str));
-      ebbrt::console->Write(std::move(buf));
-      context.Deactivate();
-      });
+#ifdef __linux__
+
+#include <stdlib.h>
+#include <fstream>
+#include "lib/fdt/libfdt.h"
+
+
+/** this is included as a ugly kludge around our current configuration model. 
+ * i.e., we need an id to statically construct ebbs before an fdt can be
+ * parsed */
+constexpr ebbrt::app::Config::StaticEbbId static_ebbs[] = {
+  {.name = "EbbManager", .id = 2},
+  {.name = "Console", .id = 5},
+  {.name = "EventManager", .id = 6},
+};
+const ebbrt::app::Config ebbrt::app::config = {
+  .space_id = 0,
+  .num_late_init = 0,
+  .late_init_ebbs = 0,
+  .num_statics = sizeof(static_ebbs) / sizeof(Config::StaticEbbId),
+  .static_ebb_ids = static_ebbs
+};
+
+
+int 
+main(int argc, char* argv[] )
+{
+  char *fdt;
+  if(argc < 2)
+  {
+      std::cout << "Usage: ./HelloEbb HelloEbb.dtb\n";
+      std::exit(1);
   }
-  for (auto& thread : threads) {
-    thread.join();
+  std::ifstream is (argv[1], std::ifstream::binary);
+  if (is) {
+    // get length of file:
+    is.seekg (0, is.end);
+    int length = is.tellg();
+    is.seekg (0, is.beg);
+
+    fdt = new char [length+length];
+
+    std::cout << "file opened, reading " << length << " characters... ";
+    // read data as a block:
+    is.read (fdt,length);
+
+    if (is)
+      std::cout << "all characters read successfully.\n";
+    else
+      std::cout << "error: only " << is.gcount() << " could be read\n";
+    is.close();
+  }else
+  {
+      std::cout << "No configuration found. Exiting\n";
+      std::exit(1);
   }
+
+  // ...fdt buffer contains the entire file...
+  ebbrt::EbbRT instance((void *)fdt);
+  ebbrt::Context context{instance};
+  context.Activate();
+  char str[] = "Hello Ebb\n";
+  auto buf = ebbrt::console->Alloc(sizeof(str));
+  std::memcpy(buf.data(), str, sizeof(str));
+  ebbrt::console->Write(std::move(buf));
+  context.Deactivate();
+
+  std::cout << "Sucessful run\n";
   return 0;
 }
 #endif
