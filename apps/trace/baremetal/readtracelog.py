b0b9b5aa250330a20712d47c35293df5fa53017b
Jim Cadden <jmcadden@bu.edu>
2014-03-24 11:36:07 -0400

    Read trace script implemented with hashtable lookupwq

--- apps/trace/baremetal/readtracelog.py
+++ apps/trace/baremetal/readtracelog.py
@@ -5,29 +5,51 @@ if len(sys.argv) != 3:
   print "Error: wrong amount of arguments."
   exit(1)
 
+def addr2line(inst):
+    try:
+        return traces[inst]
+    except KeyError:
+        cmd = "addr2line -s -f -C -e " + sys.argv[1]+' '+inst
+        fname = subprocess.check_output(cmd, shell=True).splitlines()
+        traces[inst] = fname[0]
+        return fname[0]
+
+#vars
+depth = 0
+#stacks
+times = []
+cycles = []
+insts = []
+#hashmap
+traces = {}
+
 with open(sys.argv[2]) as f:
-  depth = 0
-  times = []
-  cycles = []
-  insts = []
   for line in f:
-    data = line.split(' ')
-    fname_s = "addr2line -s -f -C -e " + sys.argv[1]+' '+data[1]
-    fname = subprocess.check_output(fname_s, shell=True).splitlines()
     if line.startswith("1"):
-      #depth += 1
+      depth += 1
+      data = line.split(' ')
+      fname = addr2line(data[1])
+      cname = addr2line(data[2])
       times.append(int(data[3]))
       cycles.append(int(data[4]))
       insts.append(int(data[5]))
-      #s = '  ' * depth
-      cname_s = "addr2line -s -f -C -i -e "+ sys.argv[1]+' '+data[2]
-      cname = subprocess.check_output(cname_s, shell=True).splitlines()
-      print ">>",fname[0],"from", cname[0],"in",cname[1]
-    if line.startswith("0"):
-      ttime = int(data[3]) - times.pop()
-      tcycles = int(data[4]) - cycles.pop()
-      tinst = int(data[5]) - insts.pop()
-      #s = '  ' * depth
-      print "<< ",fname[0],"[",tcycles,"C,",tinst,"I,",ttime,"T]"
-      #jdepth -= 1
-  print "finished."
+      print depth,">>>",fname,"from",cname
+    elif line.startswith("0"):
+      data = line.split(' ')
+      fname = addr2line(data[1])
+      if depth > 0:
+        ttime = int(data[3]) - times.pop()
+        tcycles = int(data[4]) - cycles.pop()
+        tinst = int(data[5]) - insts.pop()
+        print depth,"<<<",fname,"[",tcycles,"C,",tinst,"I,",ttime,"T]"
+      else:
+        print depth,"<<<",fname,"[?]"
+      depth -= 1
+    else:
+      # non-trace line
+      depth = 0
+      times = []
+      cycles = []
+      insts = []
+      print line
+  print "Trace print completed."
