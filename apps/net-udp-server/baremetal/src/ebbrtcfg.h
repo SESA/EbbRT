2f6a2e21904cfb605c30b482051a01ba536ceaf9
Dan Schatzberg <schatzberg.dan@gmail.com>
2014-09-10 11:51:07 -0400

    Separate out network apps

--- apps/net-udp-server/baremetal/src/ebbrtcfg.h
+++ apps/net-udp-server/baremetal/src/ebbrtcfg.h
@@ -2,11 +2,11 @@
 // Distributed under the Boost Software License, Version 1.0.
 //    (See accompanying file LICENSE_1_0.txt or copy at
 //          http://www.boost.org/LICENSE_1_0.txt)
-#ifndef APPS_NET_BAREMETAL_SRC_EBBRTCFG_H_
-#define APPS_NET_BAREMETAL_SRC_EBBRTCFG_H_
+#ifndef APPS_NET_UDP_SERVER_BAREMETAL_SRC_EBBRTCFG_H_
+#define APPS_NET_UDP_SERVER_BAREMETAL_SRC_EBBRTCFG_H_
 
 #define __EBBRT_ENABLE_FDT__ 0
 #define __EBBRT_ENABLE_DISTRIBUTED_RUNTIME__ 0
 #define __EBBRT_ENABLE_NETWORKING__ 1
 
-#endif  // APPS_NET_BAREMETAL_SRC_EBBRTCFG_H_
+#endif  // APPS_NET_UDP_SERVER_BAREMETAL_SRC_EBBRTCFG_H_
