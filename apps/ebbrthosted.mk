7d90eecd5a93cb14a2a528ccbba5d8a37b5bad4a
Dan Schatzberg <schatzberg.dan@gmail.com>
2015-03-16 20:00:53 -0400

    Update hosted build for more flexibility

--- apps/ebbrthosted.mk
+++ apps/ebbrthosted.mk
@@ -18,7 +18,8 @@ endif
 
 INCLUDES := \
 	-I $(ebbrt_hostedinc) \
-	-I $(ebbrt_commoninc) 
+	-I $(ebbrt_commoninc) \
+	-iquote $(CURDIR)
 
 CXXFLAGS := -std=c++11 $(INCLUDES) $(OPTFLAGS)
 
@@ -35,13 +36,38 @@ endif
 bm_imgdir = bm
 bm_imgs = $(addprefix ${bm_imgdir}/,$(notdir $(EBBRT_BM_IMGS)))
 
-app_objects := $(app_sources:.cc=.o)
+ebbrt_very-quiet = $(if $V, $1, @$1)
+
+ebbrt_build-cxx = CXX -MD -MT $@ -MP -Wall -Werror $(CXXFLAGS) -c -o $@ $<
+ebbrt_q-build-cxx = $(call ebbrt_quiet, $(ebbrt_build-cxx), CXX $@)
+
+%.capnp.h %.capnp.c++: %.capnp
+	capnp compile -oc++:$(CURDIR) --src-prefix=$(dir $<) $<
+
+%.capnp.o: %.capnp.c++
+	g++ -MD -MT $@ -MP -Wall -Werror $(CXXFLAGS) -c -o $@ $<
+
+%.o: %.cc
+	g++ -MD -MT $@ -MP -Wall -Werror $(CXXFLAGS) -c -o $@ $<
+
+%.o: %.cpp
+	g++ -MD -MT $@ -MP -Wall -Werror $(CXXFLAGS) -c -o $@ $<
+
+%.o: %.c++
+	g++ -MD -MT $@ -MP -Wall -Werror $(CXXFLAGS) -c -o $@ $<
+
+app_capnp_objects := $(app_capnps:.capnp=.capnp.o)
+app_capnp_cxx := $(app_capnps:.capnp=.capnp.c++)
+app_capnp_h := $(app_capnps:.capnp=.capnp.h)
+app_capnp_gens := $(app_capnp_cxx) $(app_capnp_h) $(app_capnp_objects)
 
 ${target}: $(app_objects) $(ebbrt_lib) $(bm_imgs)
 	$(CXX) $(OPTFLAGS)  -Wl,--whole-archive $(app_objects) $(ebbrt_lib) -Wl,--no-whole-archive \
 	-lboost_coroutine -lboost_context -lboost_filesystem -lboost_system \
 	-lcapnp -lkj -lfdt -ltbb  -pthread -o $@
 	@echo CREATED: $(abspath ${target})
+
+$(app_objects): $(app_capnp_objects)
 
 ${ebbrt_libdir}:
 	mkdir ${ebbrt_libdir}
@@ -61,12 +87,14 @@ ${bm_imgs}: ${bm_imgdir} ${EBBRT_BM_IMGS}
 ${EBBRT_BM_IMGS}:
 	$(MAKE) -C $(dir $@)
 
+.PRECIOUS: $(app_capnp_gens)
+
 .PHONY: distclean
 
 distclean: clean
 
 clean:
-	-$(RM) $(wildcard $(app_objects) $(target))
+	-$(RM) $(wildcard $(app_objects) $(target) $(app_capnp_gens))
 	-$(RM) -rf $(wildcard $(bm_imgdir))
 	-$(RM) -rf $(wildcard $(ebbrt_libdir))
 
@@ -74,4 +102,4 @@ clean:
 # build directories
 # ../../src : hosted source for this application
 # ../../../src: Common source for this application both baremetal and hosted
-VPATH := $(abspath ../../src):$(abspath ../../../src):$(abspath ../../../../src):$(abspath ../../../../src/hosted)
+VPATH := $(abspath ../../src):$(abspath ../../../src):$(abspath ../../../../src):$(abspath ../../../../src/hosted):$(EBBRT_APP_VPATH)
