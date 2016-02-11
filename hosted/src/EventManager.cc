d0fdbb012bd28b955c99b4cd10f3ec5daa1c251f
Jim Cadden <jmcadden@bu.edu>
2016-02-11 18:55:09 -0500

    style(EventManager): apply clang formatting

--- hosted/src/EventManager.cc
+++ hosted/src/EventManager.cc
@@ -6,34 +6,36 @@
 
 #include <ebbrt/Context.h>
 
-void ebbrt::EventManager::Spawn(MovableFunction<void()> func, 
-				ebbrt::Context *ctxt, 
-				bool force_async) {
+void ebbrt::EventManager::Spawn(MovableFunction<void()> func,
+                                ebbrt::Context* ctxt, bool force_async) {
   // movable function cannot be copied so we put it on the heap
   auto f = new MovableFunction<void()>(std::move(func));
-  // async dispatch
+// async dispatch
 #if 0
   if (ctxt == active_context)  printf("Local: "); else printf("Remote: ");
   printf("%zd->%zd\n", active_context->index_, ctxt->index_);
 #endif
- 
+
   ctxt->io_service_.post([f, this, ctxt]() {
     // create and run a new coroutine to run the event on
- ctxt->active_event_context_.coro = boost::coroutines::coroutine<void()>([f, this,ctxt](
-        boost::coroutines::coroutine<void()>::caller_type& ca) {
-      ca();
-      // store the caller for later if we need to save the context
-      ctxt->active_event_context_.caller = &ca;
-      (*f)();
-      delete f;
-    });
+    ctxt->active_event_context_.coro =
+        boost::coroutines::coroutine<void()>([f, this, ctxt](
+            boost::coroutines::coroutine<void()>::caller_type& ca) {
+          ca();
+          // store the caller for later if we need to save the context
+          ctxt->active_event_context_.caller = &ca;
+          (*f)();
+          delete f;
+        });
     ctxt->active_event_context_.coro();
   });
+
 }
 
-void ebbrt::EventManager::ActivateContext(EventManager::EventContext&& context) {
+void
+ebbrt::EventManager::ActivateContext(EventManager::EventContext&& context) {
   auto c = new EventManager::EventContext(std::move(context));
-  ebbrt::Context *ctxt = active_context;
+  ebbrt::Context* ctxt = active_context;
   ctxt->io_service_.post([c, this, ctxt]() {
     ctxt->active_event_context_ = std::move(*c);
     ctxt->active_event_context_.coro();
--- hosted/src/include/ebbrt/EventManager.h
+++ hosted/src/include/ebbrt/EventManager.h
@@ -20,14 +20,14 @@ using ebbrt::EventContext;
 const constexpr auto event_manager = EbbRef<EventManager>(kEventManagerId);
 
 class EventManager : public StaticSharedEbb<EventManager>, public CacheAligned {
-  public:
+ public:
   typedef ebbrt::EventContext EventContext;
-  void Spawn(ebbrt::MovableFunction<void()> func, ebbrt::Context *ctxt, 
-	     bool force_async=false);
-  void Spawn(ebbrt::MovableFunction<void()> func, bool force_async=false) {
-    Spawn(std::move(func),active_context,force_async);
+  void Spawn(ebbrt::MovableFunction<void()> func, ebbrt::Context* ctxt,
+             bool force_async = false);
+  void Spawn(ebbrt::MovableFunction<void()> func, bool force_async = false) {
+    Spawn(std::move(func), active_context, force_async);
   }
-  void SpawnRemote(ebbrt::MovableFunction<void()> func, ebbrt::Context *ctxt) {
+  void SpawnRemote(ebbrt::MovableFunction<void()> func, ebbrt::Context* ctxt) {
     Spawn(std::move(func), ctxt);
   }
   void ActivateContext(EventManager::EventContext&& context);
@@ -38,16 +38,16 @@ class EventManager : public StaticSharedEbb<EventManager>, public CacheAligned {
     explicit HandlerWrapper(const F& f) : f_(f) {}
     explicit HandlerWrapper(F&& f) : f_(std::move(f)) {}
     template <typename... Args> void operator()(Args&&... args) {
-      ebbrt::Context *ctxt=active_context;
-      ctxt->active_event_context_.coro =
-          boost::coroutines::coroutine<void()>(std::bind(
-	       [this,ctxt](boost::coroutines::coroutine<void()>::caller_type& ca,
-                     Args&&... args) {
-                ctxt->active_event_context_.caller = &ca;
-                ca();
-                f_(std::forward<Args>(args)...);
-              },
-              std::placeholders::_1, std::forward<Args>(args)...));
+      ebbrt::Context* ctxt = active_context;
+      ctxt->active_event_context_.coro = boost::coroutines::coroutine<void()>(
+          std::bind([this, ctxt](
+                        boost::coroutines::coroutine<void()>::caller_type& ca,
+                        Args&&... args) {
+                      ctxt->active_event_context_.caller = &ca;
+                      ca();
+                      f_(std::forward<Args>(args)...);
+                    },
+                    std::placeholders::_1, std::forward<Args>(args)...));
       ctxt->active_event_context_.coro();
     }
 
