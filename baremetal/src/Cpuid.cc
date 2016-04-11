22a58ce6c392c5c416d2096cec1164982f7ce8cb
Jim Cadden <jmcadden@bu.edu>
2016-04-11 12:20:18 -0400

    feat(Perf): Intel Performance Counter Monitor
    PerfCounter object is bound to a hardware counter and event-type. Each
    counter is core local and there is no concurrency when add/removing
    counter objects.

--- baremetal/src/Cpuid.cc
+++ baremetal/src/Cpuid.cc
@@ -3,24 +3,11 @@
 //    (See accompanying file LICENSE_1_0.txt or copy at
 //          http://www.boost.org/LICENSE_1_0.txt)
 
-#include <ebbrt/Cpuid.h>
-
 #include <cstdint>
 #include <cstring>
+#include <ebbrt/Cpuid.h>
 
 namespace {
-struct Result {
-  uint32_t eax;
-  uint32_t ebx;
-  uint32_t ecx;
-  uint32_t edx;
-};
-
-Result Cpuid(uint32_t leaf) {
-  Result r;
-  asm("cpuid" : "=a"(r.eax), "=b"(r.ebx), "=c"(r.ecx), "=d"(r.edx) : "a"(leaf));
-  return r;
-}
 
 struct VendorId {
   uint32_t ebx;
@@ -28,7 +15,7 @@ struct VendorId {
   uint32_t edx;
 };
 
-VendorId kvm_vendor_id = { 0x4b4d564b, 0x564b4d56, 0x4d };
+VendorId kvm_vendor_id = {0x4b4d564b, 0x564b4d56, 0x4d};
 
 struct CpuidBit {
   uint32_t leaf;
@@ -39,16 +26,21 @@ struct CpuidBit {
 };
 
 CpuidBit cpuid_bits[] = {
-  { 1, 2, 21, &ebbrt::cpuid::Features::x2apic },
-  { 0x40000001, 0, 6, &ebbrt::cpuid::Features::kvm_pv_eoi, &kvm_vendor_id },
-  { 0x40000001, 0, 3, &ebbrt::cpuid::Features::kvm_clocksource2,
-    &kvm_vendor_id }
-};
+    {1, 2, 21, &ebbrt::cpuid::Features::x2apic},
+    {0x40000001, 0, 6, &ebbrt::cpuid::Features::kvm_pv_eoi, &kvm_vendor_id},
+    {0x40000001, 0, 3, &ebbrt::cpuid::Features::kvm_clocksource2,
+     &kvm_vendor_id}};
 
 constexpr size_t nr_cpuid_bits = sizeof(cpuid_bits) / sizeof(CpuidBit);
 }  // namespace
 
 ebbrt::cpuid::Features ebbrt::cpuid::features;
+
+ebbrt::cpuid::Result ebbrt::cpuid::Cpuid(uint32_t leaf) {
+  Result r;
+  asm("cpuid" : "=a"(r.eax), "=b"(r.ebx), "=c"(r.ecx), "=d"(r.edx) : "a"(leaf));
+  return r;
+}
 
 void ebbrt::cpuid::Init() {
   for (size_t i = 0; i < nr_cpuid_bits; ++i) {
@@ -69,7 +61,7 @@ void ebbrt::cpuid::Init() {
     }
 
     auto res = Cpuid(bit.leaf);
-    uint32_t res_array[4] = { res.eax, res.ebx, res.ecx, res.edx };
+    uint32_t res_array[4] = {res.eax, res.ebx, res.ecx, res.edx};
     uint32_t val = res_array[bit.reg];
     features.*(bit.flag) = (val >> bit.bit) & 1;
   }
