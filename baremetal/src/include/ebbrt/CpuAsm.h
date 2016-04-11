00de920da5287b27172c90d8d935a955944aa4ab
Jim Cadden <jmcadden@bu.edu>
2016-04-11 12:20:18 -0400

    feat(CpuAsm,Cpuid): expose additional asm methods
    includes: cpuid, rdmsr, wrmsr, rdtsc, rdtscp

--- baremetal/src/include/ebbrt/CpuAsm.h
+++ baremetal/src/include/ebbrt/CpuAsm.h
@@ -5,11 +5,45 @@
 #ifndef BAREMETAL_SRC_INCLUDE_EBBRT_CPUASM_H_
 #define BAREMETAL_SRC_INCLUDE_EBBRT_CPUASM_H_
 
+#include <cstdint>
+
 namespace ebbrt {
 inline uintptr_t ReadCr3() {
   uintptr_t cr3;
   asm volatile("mov %%cr3, %[cr3]" : [cr3] "=r"(cr3));
   return cr3;
+}
+
+inline uint64_t rdtsc() {
+  uint64_t tsc;
+  asm volatile("rdtsc;"
+               "shl $32,%%rdx;"
+               "or %%rdx,%%rax"
+               : "=a"(tsc)
+               :
+               : "%rcx", "%rdx");
+  return tsc;
+}
+
+inline uint64_t rdtscp() {
+  uint64_t tsc;
+  asm volatile("rdtscp;"
+               "shl $32,%%rdx;"
+               "or %%rdx,%%rax"
+               : "=a"(tsc)
+               :
+               : "%rcx", "%rdx");
+  return tsc;
+}
+
+inline void wrmsr(uint64_t val, uint32_t msr) {
+  asm volatile("wrmsr;" : : "a"(val & 0xFFFFFFFF), "d"(val >> 32), "c"(msr));
+}
+
+inline uint64_t rdmsr(uint32_t msr) {
+  uint64_t val;
+  asm volatile("rdmsr;" : "=A"(val) : "c"(msr));
+  return val;
 }
 }
 
