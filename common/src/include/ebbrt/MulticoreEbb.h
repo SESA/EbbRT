8a1a921d63ca40dda84f40b43585d00867f3d3a8
Jim Cadden <jmcadden@bu.edu>
2016-04-19 22:03:56 -0400

    style(MulticoreEbb): Add MulticoreEbbBase

--- common/src/include/ebbrt/MulticoreEbb.h
+++ common/src/include/ebbrt/MulticoreEbb.h
@@ -1,4 +1,5 @@
 //          Copyright Boston University SESA Group 2013 - 2014.
+//
 // Distributed under the Boost Software License, Version 1.0.
 //    (See accompanying file LICENSE_1_0.txt or copy at
 //          http://www.boost.org/LICENSE_1_0.txt)
@@ -40,7 +41,7 @@ template <class T> class MulticoreEbbRoot {
   void SetEbbId(EbbId id) { id_ = id; }
   RepMapHandle GetReps() const {
     if (!id_)
-      throw std::runtime_error("No EbbRT specified in MulticoreEbbRoot");
+      throw std::runtime_error("No EbbId specified in MulticoreEbbRoot");
     struct RepMapHandle rep;
     auto found = local_id_map->Find(*rep.accessor_, id_);
     if (!found)
@@ -50,142 +51,122 @@ template <class T> class MulticoreEbbRoot {
   }
 
  private:
-  LocalIdMap::ConstAccessor accessor_;
   typedef boost::container::flat_map<size_t, T*> RepMap;
-  ebbrt::EbbId id_;
+  EbbId id_;
 };
 
-template <class T, class Root = void> class MulticoreEbb;
-template <class T, class Root> class MulticoreEbb {
+template <class T, class Root> class MulticoreEbbBase {
  public:
   static EbbRef<T> Create(Root* root, EbbId id = ebb_allocator->Allocate()) {
-    auto pair = std::make_pair(root, RepMap());
+    auto pair = std::make_pair(root, boost::container::flat_map<size_t, T*>());
     local_id_map->Insert(std::make_pair(id, std::move(pair)));
     return EbbRef<T>(id);
   }
-  static T& HandleFault(EbbId id) {
-    T* rep;
-    {
-      LocalIdMap::ConstAccessor accessor;
-      auto found = local_id_map->Find(accessor, id);
-      if (!found)
-        throw std::runtime_error("Failed to find root for MulticoreEbb");
 
-      auto pair = boost::any_cast<std::pair<Root*, RepMap>>(&accessor->second);
-      const auto& root = *(pair->first);
-      const auto& rep_map = pair->second;
-      auto it = rep_map.find(Cpu::GetMine());
-      if (it != rep_map.end()) {
-        EbbRef<T>::CacheRef(id, *it->second);
-        return *it->second;
-      }
-      // we failed to find a rep, we must construct one
-      rep = new T(root);
-    }
-
+  static std::pair<Root*, boost::container::flat_map<size_t, T*>>*
+  GetMapEntry(EbbId id) {
     LocalIdMap::Accessor accessor;
     auto found = local_id_map->Find(accessor, id);
     if (!found)
       throw std::runtime_error("Failed to find root for MulticoreEbb");
 
-    auto pair = boost::any_cast<std::pair<Root*, RepMap>>(&accessor->second);
-    auto& rep_map = pair->second;
-    rep_map[Cpu::GetMine()] = rep;
-    EbbRef<T>::CacheRef(id, *rep);
-    return *rep;
+    return boost::any_cast<
+        std::pair<Root*, boost::container::flat_map<size_t, T*>>>(
+        &accessor->second);
   }
+};
+
+template <class T, class Root = void>
+class MulticoreEbb : public MulticoreEbbBase<T, Root> {
+ public:
+  static T& HandleFault(EbbId id);
 
  private:
   typedef boost::container::flat_map<size_t, T*> RepMap;
 };
 
-template <class T> class MulticoreEbb<T, void> {
+template <class T>
+class MulticoreEbb<T, MulticoreEbbRoot<T>>
+    : public MulticoreEbbBase<T, MulticoreEbbRoot<T>> {
  public:
-  static EbbRef<T> Create(EbbId id = ebb_allocator->Allocate()) {
-    local_id_map->Insert(std::make_pair(id, RepMap()));
-    return EbbRef<T>(id);
-  }
-
-  static T& HandleFault(EbbId id) {
-    T* rep;
-    {
-      LocalIdMap::ConstAccessor accessor;
-      auto found = local_id_map->Find(accessor, id);
-      if (!found)
-        throw std::runtime_error("Failed to find root for MulticoreEbb");
-      auto rep_map = boost::any_cast<RepMap>(accessor->second);
-      auto it = rep_map.find(Cpu::GetMine());
-      if (it != rep_map.end()) {
-        EbbRef<T>::CacheRef(id, *it->second);
-        return *it->second;
-      }
-    }
-    // we failed to find a rep, we must construct one
-    rep = new T;
-    LocalIdMap::Accessor accessor;
-    auto found = local_id_map->Find(accessor, id);
-    if (!found)
-      throw std::runtime_error("Failed to find root for MulticoreEbb");
-    auto rep_map = boost::any_cast<RepMap>(accessor->second);
-    rep_map[Cpu::GetMine()] = rep;
-    EbbRef<T>::CacheRef(id, *rep);
-    return *rep;
-  }
-
- private:
-  typedef boost::container::flat_map<size_t, T*> RepMap;
-};
-
-template <class T> class MulticoreEbb<T, MulticoreEbbRoot<T>> {
- public:
+  static T& HandleFault(EbbId id);
   void SetRoot(MulticoreEbbRoot<T>* root) { root_ = root; }
   const MulticoreEbbRoot<T>* GetRoot() { return root_; }
-  static EbbRef<T> Create(MulticoreEbbRoot<T>* root,
-                          EbbId id = ebb_allocator->Allocate()) {
-    root->SetEbbId(id);
-    auto pair = std::make_pair(root, RepMap());
-    local_id_map->Insert(std::make_pair(id, std::move(pair)));
-    return EbbRef<T>(id);
-  }
-  static T& HandleFault(EbbId id) {
-    T* rep;
-    {
-      LocalIdMap::ConstAccessor accessor;
-      auto found = local_id_map->Find(accessor, id);
-      if (!found)
-        throw std::runtime_error("Failed to find root for MulticoreEbb");
-
-      auto pair = boost::any_cast<std::pair<MulticoreEbbRoot<T>*, RepMap>>(
-          &accessor->second);
-      const auto& rep_map = pair->second;
-      auto root = pair->first;
-      auto it = rep_map.find(Cpu::GetMine());
-      if (it != rep_map.end()) {
-        EbbRef<T>::CacheRef(id, *it->second);
-        return *it->second;
-      }
-      // we failed to find a rep, we must construct one
-      rep = new T();
-      rep->SetRoot(root);
-    }
-
-    LocalIdMap::Accessor accessor;
-    auto found = local_id_map->Find(accessor, id);
-    if (!found)
-      throw std::runtime_error("Failed to find root for MulticoreEbb");
-
-    auto pair = boost::any_cast<std::pair<MulticoreEbbRoot<T>*, RepMap>>(
-        &accessor->second);
-    auto& rep_map = pair->second;
-    rep_map[Cpu::GetMine()] = rep;
-    EbbRef<T>::CacheRef(id, *rep);
-    return *rep;
-  }
 
  private:
   const MulticoreEbbRoot<T>* root_;
   typedef boost::container::flat_map<size_t, T*> RepMap;
 };
+
+template <class T> class MulticoreEbb<T, void> {
+ public:
+  static EbbRef<T> Create(EbbId id = ebb_allocator->Allocate());
+  static T& HandleFault(EbbId id);
+
+ private:
+  typedef boost::container::flat_map<size_t, T*> RepMap;
+};
+
+template <class T> EbbRef<T> MulticoreEbb<T, void>::Create(EbbId id) {
+  local_id_map->Insert(std::make_pair(id, RepMap()));
+  return EbbRef<T>(id);
+}
+
+template <class T, class Root> T& MulticoreEbb<T, Root>::HandleFault(EbbId id) {
+  auto pair = MulticoreEbbBase<T, Root>::GetMapEntry(id);
+  const auto& root = *(pair->first);
+  auto rep_map = boost::any_cast<RepMap>(pair->second);
+  auto it = rep_map.find(Cpu::GetMine());
+  if (it != rep_map.end()) {
+    EbbRef<T>::CacheRef(id, *it->second);
+    return *it->second;
+  }
+  // we failed to find a rep, we must construct one
+  T* rep = new T(root);
+  auto& set_map = pair->second;
+  set_map[Cpu::GetMine()] = rep;
+  EbbRef<T>::CacheRef(id, *rep);
+  return *rep;
+}
+
+template <class T>
+T& MulticoreEbb<T, MulticoreEbbRoot<T>>::HandleFault(EbbId id) {
+  auto pair = MulticoreEbbBase<T, MulticoreEbbRoot<T>>::GetMapEntry(id);
+  auto rep_map = boost::any_cast<RepMap>(pair->second);
+  auto it = rep_map.find(Cpu::GetMine());
+  if (it != rep_map.end()) {
+    EbbRef<T>::CacheRef(id, *it->second);
+    return *it->second;
+  }
+  // we failed to find a rep, we must construct one
+  auto root = pair->first;
+  T* rep = new T();
+  root->SetEbbId(id);
+  rep->SetRoot(root);
+  auto& set_map = pair->second;
+  set_map[Cpu::GetMine()] = rep;
+  EbbRef<T>::CacheRef(id, *rep);
+  return *rep;
+}
+
+template <class T> T& MulticoreEbb<T, void>::HandleFault(EbbId id) {
+  T* rep;
+  LocalIdMap::ConstAccessor accessor;
+  auto found = local_id_map->Find(accessor, id);
+  if (!found)
+    throw std::runtime_error("Failed to find root for MulticoreEbb");
+  auto rep_map = boost::any_cast<RepMap>(accessor->second);
+  auto it = rep_map.find(Cpu::GetMine());
+  if (it != rep_map.end()) {
+    EbbRef<T>::CacheRef(id, *it->second);
+    return *it->second;
+  }
+  // we failed to find a rep, we must construct one
+  rep = new T;
+  EbbRef<T>::CacheRef(id, *rep);
+  return *rep;
+}
+
 }  // namespace ebbrt
 
 #endif  // COMMON_SRC_INCLUDE_EBBRT_MULTICOREEBB_H_
