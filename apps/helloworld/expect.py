37cde5c6a585bc73c65e6e99a95e300086189373
Dan Schatzberg <schatzberg.dan@gmail.com>
2016-04-20 10:44:40 -0400

    test(app): Add helloworld test
    Add pexpect script to check for success of a helloworld app run.
    
    Signed-off-by: Dan Schatzberg <schatzberg.dan@gmail.com>

---
+++ apps/helloworld/expect.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+import pexpect
+import sys
+import time
+
+filename = sys.argv[1]
+child = pexpect.spawn(filename)
+child.logfile = sys.stdout
+child.expect('\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}: Hello World', timeout=60)
+child.kill(2)
