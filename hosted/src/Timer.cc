dbfadbb70bdce85cd9405afcacab575fc78e6e13
Jim Cadden <jmcadden@bu.edu>
2016-06-08 09:34:45 -0400

    fix(Timer): Enable blocking on hosted fire event
    Pull request: #131
    Approved by: dschatzberg

--- hosted/src/Timer.cc
+++ hosted/src/Timer.cc
@@ -3,6 +3,7 @@
 //    (See accompanying file LICENSE_1_0.txt or copy at
 //          http://www.boost.org/LICENSE_1_0.txt)
 #include <ebbrt/Context.h>
+#include <ebbrt/EventManager.h>
 #include <ebbrt/Timer.h>
 
 const constexpr ebbrt::EbbId ebbrt::Timer::static_id;
@@ -17,21 +18,20 @@ void ebbrt::Timer::StopTimer() { EBBRT_UNIMPLEMENTED(); }
 
 void ebbrt::Timer::Start(Hook& hook, std::chrono::microseconds timeout,
                          bool repeat) {
-  std::shared_ptr<boost::asio::deadline_timer> t(
-      new boost::asio::deadline_timer(
-          active_context->io_service_,
-          boost::posix_time::microseconds(timeout.count())));
+  auto t = std::make_shared<boost::asio::deadline_timer>(
+      active_context->io_service_,
+      boost::posix_time::microseconds(timeout.count()));
 
-  t->async_wait(
+  t->async_wait(EventManager::WrapHandler(
       [&hook, t, repeat, timeout](const boost::system::error_code& e) {
         if (e) {
           ebbrt::kabort("ASIO Error: %d\n", e.value());
         }
-        hook.Fire();
         if (repeat) {
           timer->Start(hook, timeout, repeat);
         }
-      });
+        hook.Fire();
+      }));
 }
 
 void ebbrt::Timer::Stop(Hook& hook) { EBBRT_UNIMPLEMENTED(); }
