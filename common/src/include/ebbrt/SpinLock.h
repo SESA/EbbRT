a4c506c07e0e2e00cf76b4cca6446a22d8b47544
Jim Cadden <jmcadden@bu.edu>
2016-06-08 10:24:37 -0400

    refactor(SpinLock): relocate to common
    Pull request: #131
    Approved by: dschatzberg

--- common/src/include/ebbrt/SpinLock.h
+++ common/src/include/ebbrt/SpinLock.h
@@ -2,8 +2,8 @@
 // Distributed under the Boost Software License, Version 1.0.
 //    (See accompanying file LICENSE_1_0.txt or copy at
 //          http://www.boost.org/LICENSE_1_0.txt)
-#ifndef BAREMETAL_SRC_INCLUDE_EBBRT_SPINLOCK_H_
-#define BAREMETAL_SRC_INCLUDE_EBBRT_SPINLOCK_H_
+#ifndef COMMON_SRC_INCLUDE_EBBRT_SPINLOCK_H_
+#define COMMON_SRC_INCLUDE_EBBRT_SPINLOCK_H_
 
 #include <atomic>
 
@@ -24,4 +24,4 @@ class SpinLock {
 };
 }  // namespace ebbrt
 
-#endif  // BAREMETAL_SRC_INCLUDE_EBBRT_SPINLOCK_H_
+#endif  // COMMON_SRC_INCLUDE_EBBRT_SPINLOCK_H_
