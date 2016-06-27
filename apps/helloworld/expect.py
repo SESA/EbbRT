3e88e1dea7d2dfdd391c2fb4bd700385663bc205
Jim Cadden <jmcadden@bu.edu>
2016-06-27 16:00:11 -0400

    chore(ci): build environment Dockerfile
    Pull request: #139
    Approved by: jmcadden

--- apps/helloworld/expect.py
+++ apps/helloworld/expect.py
@@ -8,4 +8,5 @@ filename = sys.argv[1]
 child = pexpect.spawn(filename)
 child.logfile = sys.stdout
 child.expect('\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}: Hello World', timeout=60)
-child.kill(2)
+child.sendcontrol('c')
+child.wait()
