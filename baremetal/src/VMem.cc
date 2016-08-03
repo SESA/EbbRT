10cb593c9d1841a8061c0c2b2f81c8dd3bba21b6
Han Dong <handong@bu.edu>
2016-08-03 17:18:57 -0400

    fix(GeneralPurposeAllocator): fault handler for large regions
    There was no page fault handler for large memory pages
    in multi-coreand they were set to nullptrs, which cause
    a secondary page fault. This fix keeps a table of virtual
    to physical page mappings and uses it to do the same
    mapping on cores that suffer the fault on a virtual address.
    
    Pull request: #147
    Approved by: dschatzberg

--- baremetal/src/VMem.cc
+++ baremetal/src/VMem.cc
@@ -87,6 +87,27 @@ void ebbrt::vmem::MapMemory(Pfn vfn, Pfn pfn, uint64_t length) {
                     });
 }
 
+// traverses per core page table and backs vaddr with physical pages
+// in pfn
+void ebbrt::vmem::MapMemoryLarge(uintptr_t vaddr, Pfn pfn, uint64_t length) {
+  auto pte_root = Pte(ReadCr3());
+  TraversePageTable(
+      pte_root, vaddr, vaddr + length, 0, 4,
+      [=](Pte& entry, uint64_t base_virt, size_t level) {
+        kassert(!entry.Present());
+        entry.Set(pfn.ToAddr() + (base_virt - vaddr), level > 0);
+        std::atomic_thread_fence(std::memory_order_release);
+      },
+      [](Pte& entry) {
+        auto page = page_allocator->Alloc();
+        kbugon(page == Pfn::None());
+        auto page_addr = page.ToAddr();
+        new (reinterpret_cast<void*>(page_addr)) Pte[512];
+        entry.SetNormal(page_addr);
+        return true;
+      });
+}
+
 void ebbrt::vmem::EnableRuntimePageTable() {
   asm volatile("mov %[page_table], %%cr3"
                :
