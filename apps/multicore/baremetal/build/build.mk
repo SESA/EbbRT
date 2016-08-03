1d1a8c02180dae3cfed22aeb511aad93d3087132
Jim Cadden <jmcadden@bu.edu>
2016-08-03 18:12:31 -0400

    chore(all): remove deprecated apps and makefiles
    Deletes files dependant/part-of the 'old' build system
    
    Pull request: #149
    Approved by: dschatzberg

--- apps/multicore/baremetal/build/build.mk
+++
@@ -1,8 +0,0 @@
-MYDIR := $(dir $(lastword $(MAKEFILE_LIST)))
-
-EBBRT_TARGET := multicore
-EBBRT_APP_OBJECTS := multicore.o
-EBBRT_APP_VPATH := $(abspath $(MYDIR)../src)
-EBBRT_CONFIG := $(abspath $(MYDIR)../src/ebbrtcfg.h)
-
-include $(abspath ../../../../ebbrtbaremetal.mk)
--- apps/net-tcp-client/baremetal/build/build.mk
+++
@@ -1,8 +0,0 @@
-MYDIR := $(dir $(lastword $(MAKEFILE_LIST)))
-
-EBBRT_TARGET := net
-EBBRT_APP_OBJECTS := net.o
-EBBRT_APP_VPATH := $(abspath $(MYDIR)../src)
-EBBRT_CONFIG := $(abspath $(MYDIR)../src/ebbrtcfg.h)
-
-include $(abspath ../../../../ebbrtbaremetal.mk)
