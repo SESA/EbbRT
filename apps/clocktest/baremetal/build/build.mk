5fb6e986a782759f63f33afd1d6ae75dd3b6a7a7
Jim Cadden <jmcadden@bu.edu>
2016-08-03 18:12:31 -0400

    chore(all): remove deprecated apps and makefiles
    Deletes files dependant/part-of the 'old' build system

--- apps/clocktest/baremetal/build/build.mk
+++
@@ -1,8 +0,0 @@
-MYDIR := $(dir $(lastword $(MAKEFILE_LIST)))
-
-EBBRT_TARGET := clocktest
-EBBRT_APP_OBJECTS := clocktest.o
-EBBRT_APP_VPATH := $(abspath $(MYDIR)../src)
-EBBRT_CONFIG := $(abspath $(MYDIR)../src/ebbrtcfg.h)
-
-include $(abspath ../../../../ebbrtbaremetal.mk)
