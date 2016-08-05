a5e6ade138bc3adce36328ab6f0e0e6411ee2a1a
Dan Schatzberg <schatzberg.dan@gmail.com>
2016-08-05 11:44:11 -0700

    fix(build): Use CMAKE_SYSROOT instead of FIND_ROOT
    The CMAKE_SYSROOT variable effects more than just where cmake looks for
    libraries and headers (as CMAKE_FIND_ROOT_PATH does). It also effects
    the default install path as well as where cmake looks for
    Find{package,library,etc.} modules.
    
    Signed-off-by: Dan Schatzberg <schatzberg.dan@gmail.com>
    
    Pull request: #153
    Approved by: dschatzberg

--- baremetal/cmake/ebbrt.cmake
+++ baremetal/cmake/ebbrt.cmake
@@ -9,18 +9,19 @@ if (NOT IS_DIRECTORY $ENV{EBBRT_SYSROOT})
 endif()
 
 # where is the target environment
-SET(CMAKE_FIND_ROOT_PATH  $ENV{EBBRT_SYSROOT})
+SET(CMAKE_SYSROOT  $ENV{EBBRT_SYSROOT})
 
 # specify the cross compiler
-SET(CMAKE_C_COMPILER   ${CMAKE_FIND_ROOT_PATH}/usr/bin/x86_64-pc-ebbrt-gcc)
-SET(CMAKE_CXX_COMPILER ${CMAKE_FIND_ROOT_PATH}/usr/bin/x86_64-pc-ebbrt-g++)
-SET(CMAKE_ASM_COMPILER ${CMAKE_FIND_ROOT_PATH}/usr/bin/x86_64-pc-ebbrt-g++)
-SET(CMAKE_AR ${CMAKE_FIND_ROOT_PATH}/usr/bin/x86_64-pc-ebbrt-gcc-ar CACHE FILEPATH "Archiver")#
-SET(CMAKE_RANLIB ${CMAKE_FIND_ROOT_PATH}/usr/bin/x86_64-pc-ebbrt-gcc-ranlib CACHE FILEPATH "Archiver")
+SET(CMAKE_C_COMPILER   ${CMAKE_SYSROOT}/usr/bin/x86_64-pc-ebbrt-gcc)
+SET(CMAKE_CXX_COMPILER ${CMAKE_SYSROOT}/usr/bin/x86_64-pc-ebbrt-g++)
+SET(CMAKE_ASM_COMPILER ${CMAKE_SYSROOT}/usr/bin/x86_64-pc-ebbrt-g++)
+SET(CMAKE_AR ${CMAKE_SYSROOT}/usr/bin/x86_64-pc-ebbrt-gcc-ar CACHE FILEPATH "Archiver")
+SET(CMAKE_RANLIB ${CMAKE_SYSROOT}/usr/bin/x86_64-pc-ebbrt-gcc-ranlib CACHE FILEPATH "Archiver")
 
 SET(CMAKE_CXX_FLAGS  "${CMAKE_CXX_FLAGS} -std=gnu++14" )
 
-SET(CMAKE_SYSTEM_PREFIX_PATH ${CMAKE_FIND_ROOT_PATH}/usr)
+SET(CMAKE_STAGING_PREFIX ${CMAKE_SYSROOT}/usr)
+SET(CMAKE_SYSTEM_PREFIX_PATH ${CMAKE_SYSROOT}/usr)
 
 # search for programs in the build host directories
 SET(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
