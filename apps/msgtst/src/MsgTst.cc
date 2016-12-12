6a56c09991468179324dac32890ff370210d122a
Jim Cadden <jmcadden@bu.edu>
2016-12-12 15:44:08 -0500

    feat(apps): Add messenger test application

---
+++ apps/msgtst/src/MsgTst.cc
@@ -0,0 +1,122 @@
+//          Copyright Boston University SESA Group 2013 - 2016.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+#include "MsgTst.h"
+#include <ebbrt/UniqueIOBuf.h>
+#include <ebbrt/LocalIdMap.h>
+#include <ebbrt/Future.h>
+
+// This is *IMPORTANT*, it allows the messenger to resolve remote HandleFaults
+EBBRT_PUBLISH_TYPE(, MsgTst);
+
+using namespace ebbrt;
+
+// We don't store anything in the GlobalIdMap, just return the EbbRef
+EbbRef<MsgTst> MsgTst::Create(EbbId id) { return EbbRef<MsgTst>(id); }
+
+// This Ebb is implemented with one representative per machine
+MsgTst &MsgTst::HandleFault(EbbId id) {
+  {
+    // First we check if the representative is in the LocalIdMap (using a
+    // read-lock)
+    LocalIdMap::ConstAccessor accessor;
+    auto found = local_id_map->Find(accessor, id);
+    if (found) {
+      auto &rep = *boost::any_cast<MsgTst *>(accessor->second);
+      EbbRef<MsgTst>::CacheRef(id, rep);
+      return rep;
+    }
+  }
+
+  MsgTst *rep;
+  {
+    // Try to insert an entry into the LocalIdMap while holding an exclusive
+    // (write) lock
+    LocalIdMap::Accessor accessor;
+    auto created = local_id_map->Insert(accessor, id);
+    if (unlikely(!created)) {
+      // We raced with another writer, use the rep it created and return
+      rep = boost::any_cast<MsgTst *>(accessor->second);
+    } else {
+      // Create a new rep and insert it into the LocalIdMap
+      rep = new MsgTst(id);
+      accessor->second = rep;
+    }
+  }
+  // Cache the reference to the rep in the local translation table
+  EbbRef<MsgTst>::CacheRef(id, *rep);
+  return *rep;
+}
+
+MsgTst::MsgTst(EbbId ebbid) : Messagable<MsgTst>(ebbid) {}
+
+std::unique_ptr<MutIOBuf> MsgTst::RandomMsg(size_t bytes) {
+  std::random_device rd;
+  std::default_random_engine rng(rd());
+  std::uniform_int_distribution<> dist(0, sizeof(alphanum) / sizeof(*alphanum) -
+                                              2);
+  std::string str;
+  str.reserve(bytes);
+  std::generate_n(std::back_inserter(str), bytes,
+                  [&]() { return alphanum[dist(rng)]; });
+
+  auto buf = MakeUniqueIOBuf(bytes);
+  auto dp = buf->GetMutDataPointer();
+  auto i = buf->Length();
+  while (i--) {
+    auto ptr = dp.Data() + i;
+    *ptr = alphanum[dist(rng)];
+  };
+  return std::move(buf);
+}
+
+std::vector<ebbrt::Future<uint32_t>> MsgTst::SendMessages(Messenger::NetworkId nid,
+                                                 uint32_t count, size_t size) {
+  uint32_t id;
+  assert(size >= sizeof(uint32_t));
+  assert(count <= std::numeric_limits<uint32_t>::max());
+
+  std::vector<ebbrt::Future<uint32_t> > ret;
+  {
+    std::lock_guard<std::mutex> guard(m_);
+    id = id_; // Get a new id (always even)
+    id_ += 2 * count;
+  }
+  while(count--){
+    Promise<uint32_t> promise;
+    bool inserted;
+    auto f = promise.GetFuture();
+    ret.push_back(std::move(f));
+    std::tie(std::ignore, inserted) =
+        promise_map_.emplace(id, std::move(promise));
+    assert(inserted);
+    auto buf = RandomMsg(size);
+    auto dp = buf->GetMutDataPointer();
+    dp.Get<uint32_t>() = id + 1; // Ping messages are odd
+    id += 2;
+    SendMessage(nid, std::move(buf));
+  }
+   return ret;
+}
+
+void MsgTst::ReceiveMessage(Messenger::NetworkId nid,
+                            std::unique_ptr<IOBuf> &&iobuf) {
+  auto dp = iobuf->GetDataPointer();
+  auto id = dp.Get<uint32_t>();
+  // Ping messages use id + 1, so they are always odd
+  if (id & 1) {
+    // Received Ping
+    auto buf = MakeUniqueIOBuf(sizeof(uint32_t));
+    auto dp = buf->GetMutDataPointer();
+    dp.Get<uint32_t>() = id - 1; // Send back with the original id
+    SendMessage(nid, std::move(buf));
+  } else {
+    // Received Pong, lookup in the hash table for our promise and set it
+    std::lock_guard<std::mutex> guard(m_);
+    auto it = promise_map_.find(id);
+    assert(it != promise_map_.end());
+    it->second.SetValue(id);
+    promise_map_.erase(it);
+  }
+}
---
+++ apps/msgtst/src/hosted/msgtst.cc
@@ -0,0 +1,84 @@
+//          Copyright Boston University SESA Group 2013 - 2016.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+
+/* Messenger Test
+ * Send fixed-size messages from the front-end to back-end and wait for
+ * acks from back-end.
+ *
+ * Usage:
+ * ./msgtst <msg_size [B]> <msg_count>
+ *
+ * */
+#include <signal.h>
+
+#include <boost/filesystem.hpp>
+
+#include <ebbrt/Future.h>
+#include <ebbrt/GlobalIdMap.h>
+#include <ebbrt/Runtime.h>
+#include <ebbrt/StaticIds.h>
+#include <ebbrt/hosted/Context.h>
+#include <ebbrt/hosted/ContextActivation.h>
+#include <ebbrt/hosted/NodeAllocator.h>
+
+#include "../MsgTst.h"
+
+using namespace ebbrt;
+
+int main(int argc, char** argv) {
+  auto bindir = boost::filesystem::system_complete(argv[0]).parent_path() /
+                "/bm/msgtst.elf32";
+
+  ebbrt::Runtime runtime;
+  ebbrt::Context c(runtime);
+  boost::asio::signal_set sig(c.io_service_, SIGINT);
+  {
+    ebbrt::ContextActivation activation(c);
+
+  uint32_t msg_size;
+  uint32_t msg_count;
+  if (argc == 1) {
+    // prompt user for size/amount
+    std::cout << "Message size (bytes): ";
+    std::cin >> msg_size;
+    std::cout << "Message count: ";
+    std::cin >> msg_count;
+  } else if (argc == 2) {
+    // default amount to 1
+    msg_size = std::stoi(argv[1]);
+    msg_count = 1;
+  } else {
+    msg_size = std::stoi(argv[1]);
+    msg_count = std::stoi(argv[2]);
+  }
+
+  if (!msg_size || !msg_count)
+    exit(0);
+    // ensure clean quit on ctrl-c
+    sig.async_wait([&c](const boost::system::error_code& ec,
+                        int signal_number) { c.io_service_.stop(); });
+
+    try {
+      auto node_desc = node_allocator->AllocateNode(bindir.string(), 2, 2, 2);
+      node_desc.NetworkId().Then(
+          [msg_size, msg_count](Future<Messenger::NetworkId> f) {
+            auto nid = f.Get();
+            auto msgtst_ebb = MsgTst::Create();
+            if (msg_size > 0) {
+              std::cout << "Sending " << msg_count << " " << msg_size
+                        << "B message..." << std::endl;
+              msgtst_ebb->SendMessages(nid, 1, msg_size)[0].Block();
+              std::cout << "test successful" << std::endl;
+            }
+            return;
+          });
+    } catch (std::runtime_error& e) {
+      std::cout << e.what() << std::endl;
+      exit(1);
+    }
+  }
+  c.Run();
+  return 0;
+}
