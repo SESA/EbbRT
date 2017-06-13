db6cd90978ff7d5b5806816cdf12fb5a0ea22842
Jonathan Appavoo <jappavoo@bu.edu>
2017-06-13 09:27:49 -0400

    feat(utils) added ability to unconditionally cleanup
    if you pass an argument to contrib/clean_running_apps.sh
    then it will skip the prompt and do the cleanup unconditionally

--- contrib/clean_running_apps.sh
+++ contrib/clean_running_apps.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 # Manual clean-up of docker networks and containers created by the EbbRT NodeAllocator
-
-read -p "Remove all of your ($USER) ebbrt networks and nodes. Continue [Y]es, [n]o? " c
-case "$c" in 
-  n|N ) exit 0;
-esac
+if [[ -z $1 ]]; then
+  read -p "Remove all of your ($USER) ebbrt networks and nodes. Continue [Y]es, [n]o? " c
+  case "$c" in 
+    n|N ) exit 0;
+  esac
+fi
 
 echo "Removing docker containers..."
 docker ps | grep ebbrt-$(id -u) | cut -d ' ' -f 1 | while read id; do docker kill $id; done
